37d5fbfae8254746d9592ce7a16b09e4f206d9f0
baolo073 <nguyentam91gcd@gmail.com>
2018-03-23 14:50:21 +0700

    chạy tốt

---
+++ library/src/main/java/pt/content/helper/RSHelper.java
@@ -0,0 +1,37 @@
+package pt.content.helper;
+
+import android.content.Context;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class RSHelper {
+    public static boolean loaded = false;
+
+    public static void showOnAction(AppCompatActivity context) {
+    }
+    public static void showOnAction(PreferenceActivity context) {
+    }
+
+    public static boolean showOnBackpress(AppCompatActivity context) {
+            return true;
+    }
+    public static boolean showOnBackpress(PreferenceActivity context) {
+            return true;
+    }
+
+    public static void onStart(Context context) {
+    }
+
+    public static void onStop(Context context) {
+
+    }
+
+    public static void actionStart(Context context) {
+    }
+
+    public static boolean isPremium(Context context) {
+        return false;
+    }
+
+}
